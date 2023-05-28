67698458edac30bd28ba9080120cd886438ccfba
kisio <derrickkisio@gmail.com>
2023-05-28 11:40:05 +0300

    Header & Logout

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="">Add Restuarant</a>
+        <a href="">Update Resturant</a>
+        <a href="" @click="logout">logout</a>
+
+    </div>
+</template>
+
+<script>
+export default{
+    name:'HeaderS',
+    methods:{
+        logout(){
+            localStorage.clear();
+            this.$router.push('/Login');
+        }
+    }
+}
+</script>
+
+
+<style>
+.nav{
+    background-color: rgb(130, 83, 212);
+    overflow:hidden;
+    color:white;
+
+}
+.nav a{
+    float: left;
+    color:white;
+    padding: 14px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 20px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    
+}
+a:hover{
+    color: rgb(126, 218, 227);
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,14 @@
 <template>
+    <Header/>
     <h1>Hello, welcome to the home page</h1>
  </template>
  <script>
+ import Header from './Header.vue'
  export default {
      name: 'HomePage',
+     components:{
+        Header
+     },
      mounted() {
         let user = localStorage.getItem('user-info');
         if (!user) {
